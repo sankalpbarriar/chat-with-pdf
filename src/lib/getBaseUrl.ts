7b626f185a236ad71815d0b0d022cd687510a711
sankalpbarriar <114381745+sankalpbarriar@users.noreply.github.com>
2025-03-23 02:12:00 +0530

    bug fixes

--- src/lib/getBaseUrl.ts
+++ src/lib/getBaseUrl.ts
@@ -1,5 +1,5 @@
 const getBaseUrl = () =>
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : `https://${process.env.VERCEL_URL}`;
+    : "https://pdf-speek-pro.vercel.app/";
 export default getBaseUrl;
